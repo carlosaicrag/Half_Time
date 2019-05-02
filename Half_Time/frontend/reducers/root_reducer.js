9c3536ff7cc9a43687da951d820ed732a7374422
Carlos Garcia <carlosaicrag@gmail.com>
2019-05-01 17:04:49 -0700

    took old halftime out

--- Half_Time/frontend/reducers/root_reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import sessionReducer from './session'
-import UiReducer from './ui_reducer'
-
-export default combineReducers({
-    // entities: entitiesReducer,
-    session: sessionReducer,
-    ui: UiReducer
-})
