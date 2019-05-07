a001c763ca44cc419dd2fb4ba04779a77d849729
Carlos Garcia <carlosaicrag@gmail.com>
2019-05-07 11:20:06 -0700

    fix aspect ratio on my main page pictures

--- frontend/components/home/story_home_feed.jsx
+++ frontend/components/home/story_home_feed.jsx
@@ -6,7 +6,10 @@ const StoryHomeFeed = props => {
 
     return(
         <div className="story-home-feed">
-           <div className="story-image-home-feed-container"><img className="story-image-home-feed" src={story.photoUrl} alt={story.title}/></div>
+        
+            <div className="story-image-home-feed-container">
+                <img className="story-image-home-feed" src={story.photoUrl} alt={story.title}/>
+            </div>
 
             <Link to={`/${story.id}`} className="story-details-home-feed">
                 <div className="story-details-title-home-feed">{story.title}</div>
